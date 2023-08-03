d16211504392aec82c8d370680425645dc3bcb91
Saumya Rai <saumyarai@RGGYSMAC-48.local>
2023-08-03 21:48:27 +0530

    hotfix: page title changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import { useEffect } from "react";
 import NavBar from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "Buy Saumya A Cake"
+ }, [])
+
   return <div className="App">
     <NavBar />
   </div>;
